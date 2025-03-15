4bae94881d0329124ff1b9b6e360cfae2d4115d5
SlashNephy <slashnephy@hatena.ne.jp>
2025-03-15 17:12:38 +0900

    vitestConfig

--- src/framework/vitest.ts
+++ src/framework/vitest.ts
@@ -1,9 +1,26 @@
 import type { Linter } from 'eslint'
+import vitest from '@vitest/eslint-plugin'
 
 /**
  * vitest 関連の eslint プリセット
  */
-module.exports = {
-  plugins: ['vitest'],
-  rules: {},
-} satisfies Linter.Config
+export const vitestConfig: Linter.Config = {
+  files: [
+    '**/*.test.{js,mjs,cjs,jsx,ts,mts,cts,tsx}',
+    '**/test/**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}',
+  ],
+  plugins: { vitest },
+  rules: {
+    ...vitest.configs.recommended.rules,
+  },
+  settings: {
+    vitest: {
+      typecheck: true,
+    },
+  },
+  languageOptions: {
+    globals: {
+      ...vitest.environments.env.globals,
+    },
+  },
+}
