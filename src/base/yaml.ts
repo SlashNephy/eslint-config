e9da01a6cb3c74cca2cb9759b12fbfc72e5dcf98
SlashNephy <slashnephy@hatena.ne.jp>
2026-01-22 17:04:26 +0900

    update config

--- src/base/yaml.ts
+++ src/base/yaml.ts
@@ -1,24 +1,18 @@
 import { defineConfig } from 'eslint/config'
-import ymlPlugin from 'eslint-plugin-yml'
+import { configs as ymlConfig } from 'eslint-plugin-yml'
 
 export const yaml = defineConfig(
   {
     name: 'eslint-plugin-yml',
     files: ['**/*.{yml,yaml}'],
     extends: [
-      ymlPlugin.configs['flat/standard'],
+      ymlConfig.recommended,
     ],
-    rules: {
-      'yml/quotes': ['error', { prefer: 'double' }],
-    },
   },
   {
     name: 'eslint-plugin-yml (GitHub Workflow)',
     files: ['.github/workflows/*.{yml,yaml}'],
     rules: {
-      // ダブルクォートが使えない場合があるのでシングルに統一
-      // https://github.com/actions/runner/issues/866
-      'yml/quotes': ['error', { prefer: 'single' }],
       // workflow_dispatch: のような空のマップを許可する
       'yml/no-empty-mapping-value': 'off',
     },
